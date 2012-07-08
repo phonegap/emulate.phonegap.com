e2dc5a542890647663cc545c985b035b8e39f46e
Michael Brooks <michael@michaelbrooks.ca>
2012-07-08 16:45:47 -0400

    Refactor public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,53 @@
-var netherworld = {
+var app = {
+    initialize: function() {
+        this.bind();
+        this.run();
+    },
+    bind: function() {
+        document.getElementById("go").addEventListener("click", function () {
+            var address = document.getElementById("address");
+            netherworld.goto(address.value);
+        });
+    },
+    run: function() {
+        this.showLoading(true);
+        this.checkDependencies();
+        this.loadApp();
+        this.showLoading(false);
+    },
+    checkDependencies: function() {
+        if (!this.isSupportedBrowser()) {
+            this.show('browser-warning');
+        }
+        else if (!this.hasRipplez()) {
+            this.show('ripple-warning');
+        }
+    },
+    hasDependencies: function() {
+        return !this.visible('browser-warning') && !this.visible('ripple-warning');
+    },
+    isSupportedBrowser: function() {
+        return window.chrome;
+    },
     hasRipplez: function () {
+        // we do not use window.chrome.isInstalled(id)
+        // because Ripple has two IDs (hosted and Chrome Store)
+        // and local dev installations have unique IDs.
         return document.getElementById("tinyhippos-injected");
     },
+    loadApp: function() {
+        if (!this.hasDependencies()) {
+            return;
+        }
 
+        if (window.location.search) {
+            var uri = decodeURI(window.location.search.split("=")[1]);
+            this.goto(uri);
+        }
+        else {
+            this.show("navigate");
+        }
+    },
     goto: function (uri) {
         //TODO: Test if window.chrome exists on chromium
         if (window.chrome && netherworld.hasRipplez()) {
@@ -20,37 +65,25 @@ var netherworld = {
             //window.location.href = uri;
         }
     },
-
     show: function (id) {
         document.getElementById(id).setAttribute("style", "");
     },
-
     hide: function (id) {
         document.getElementById(id).setAttribute("style", "display: none");
+    },
+    visible: function(id) {
+        return (document.getElementById(id).style.display === '');
+    },
+    showLoading: function(loading) {
+        if (loading) {
+           this.show('loading');
+        }
+        else {
+            this.hide('loading');
+        }
     }
-
 };
 
 window.addEventListener("load", function () {
-    if (!window.chrome) {
-        console.log("asdf");
-        netherworld.show("browser-warning");
-    }
-    else if (!netherworld.hasRipplez()) {
-        console.log("ffda");
-        netherworld.show("ripple-warning");
-    }
-    else {
-        netherworld.show("navigate");
-
-        if (window.location.search) {
-            var uri = decodeURI(window.location.search.split("=")[1]);
-            netherworld.goto(uri);
-        }
-    }
-
-    document.getElementById("go").addEventListener("click", function () {
-        var address = document.getElementById("address");
-        netherworld.goto(address.value);
-    });
+    app.initialize();
 });
